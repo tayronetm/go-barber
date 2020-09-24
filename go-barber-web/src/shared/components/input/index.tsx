986694b8c6455f35a1aa1db330dfcc2c2a38b3e7
Monteiro <tayrone.n.monteiro@accenture.com>
2020-09-24 06:25:29 -0300

    Usability and usecallbacks

--- go-barber-web/src/shared/components/input/index.tsx
+++ go-barber-web/src/shared/components/input/index.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useCallback, useState, useRef } from 'react';
 import { Container } from './styles';
 import { IconBaseProps } from 'react-icons';
 import { useField }  from '@unform/core';
@@ -8,12 +8,47 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [hasValue, setHasValue] = useState(false);
   const { fieldName, defaultValue, error, registerField } = useField(name);
 
+  //Executada apenas uma vez utilizar sempre que 
+  //for preciso criar uma função dentro do componente
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    // if (inputRef.current?.value) {
+    //   setHasValue(true);
+    // } else {
+    //   setHasValue(false);
+    // }
+
+    //suggar (Se o input ref tiver valor, true senão false)
+    setHasValue(!!inputRef.current?.value)
+  }, [])
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(false)
+  }, [])
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value'
+    })
+  }, [fieldName, registerField]);
+
   return (
-    <Container>
+    <Container hasValue={hasValue} isFocused={isFocused}>
       { Icon && <Icon size={20} />}
-      <input {...rest}/>
+      <input 
+      onFocus={() => setIsFocused(true)}
+      onBlur={handleInputBlur}
+      defaultValue={defaultValue}
+      ref={inputRef} 
+      {...rest}/>
     </Container>
   )
 }
